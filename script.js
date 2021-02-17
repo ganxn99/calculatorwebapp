dd9bb15c37f852209f31ecc604214753b6f8ca77
phyllisgan <ganxn99@gmail.com>
2021-02-16 22:27:06 -0600

    hosting website

---
+++ script.js
@@ -0,0 +1,27 @@
+function getPrev(){
+	return document.getElementById("preval").innerText;
+}
+
+alert(getPrev());
+
+// function printPrev(value){
+//     document.getElementById("preval").innerText = value; 
+// }
+
+// printPrev(99+9+8);
+
+function display(num){
+    document.getElementById("curval").value+=num 
+}
+
+function solve() 
+{ 
+    let x = document.getElementById("result").value 
+    let y = eval(x) 
+    document.getElementById("result").value = y 
+} 
+
+function clr() 
+{ 
+    document.getElementById("result").value = "" 
+} 
