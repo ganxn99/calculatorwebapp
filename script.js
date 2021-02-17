9ed4d883c6c2a709a7d8d62ac9573dcf7110c20e
phyllisgan <ganxn99@gmail.com>
2021-02-17 14:44:50 -0600

    adding back javascript

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-// function getPrev(){
-// 	return document.getElementById("preval").innerText;
-// }
-
-// alert(getPrev());
-
-// function printPrev(value){
-//     document.getElementById("preval").innerText = value; 
-// }
-
-// printPrev(99+9+8);
-
-function display(num){
-    document.getElementById("curval").value+=num 
+function getPrev(){
+	return document.getElementById("preval").innerText;
 }
 
-function solve() 
-{ 
-    let x = document.getElementById("result").value 
-    let y = eval(x) 
-    document.getElementById("result").value = y 
-} 
+alert(getPrev());
+function printPrev(value){
+    document.getElementById("preval").innerText = value; 
+}
 
-function clr() 
-{ 
+function getResult(){
+	return document.getElementById("curval").innerText;
+}
+
+function printResult(num){
+	if(num==""){
+		document.getElementById("curval").innerText=num;
+	}
+	else{
+		document.getElementById("curval").innerText=getFormattedNumber(num);
+	}	
+}
+
+function dis(val){
+     document.getElementById("preval").value+=val 
+}
+
+function cal() { 
+    let x = document.getElementById("curval").value 
+    let y = eval(x) 
+    document.getElementById("preval").value = y 
+    } 
+
+function clr() { 
     document.getElementById("result").value = "" 
 } 
+
